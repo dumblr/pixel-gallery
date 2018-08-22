0aefa922f78b9bb60f2bdfc1a7f602278ebf9eba
Nick Beattie <nick@nickbytes.com>
2018-08-22 17:06:33 -0400

    feat: messing around

--- src/components/ArtworkGrid/index.js
+++ src/components/ArtworkGrid/index.js
@@ -1,39 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 // 800px x 450px
 
 const bgColor = (helper, i) => {
   if (helper % 2 === 0) {
     if (i % 2 === 0) {
-      return "rgba(142, 144, 147, 0.600)";
+      return "rgba(255,255,255,1)";
     }
     return "rgba(142, 144, 147, 0.2)";
   }
   if (i % 2 === 0) {
     return "rgba(142, 144, 147, 0.2)";
   }
-  return "rgba(142, 144, 147, 0.600)";
+  return "rgba(255,255,255,1)";
 };
+
+const Block = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: ${props => bgColor(props.helper, props.i)};
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
 
 const Blocks = ({ helper }) =>
   Array.from(Array(80)).map((x, i) => (
-    <div
-      name={x}
-      key={i}
-      style={{
-        width: "10px",
-        height: "10px",
-        backgroundColor: bgColor(helper, i)
-      }}
-    />
+    <Block key={i} name={x} helper={helper} i={i} />
   ));
 
 const ArtRow = ({ helper }) => (
-  <div style={{ display: "flex" }}>
+  <Row>
     <Blocks helper={helper} />
-  </div>
+  </Row>
 );
 
 const ArtworkGrid = () =>
   Array.from(Array(45)).map((x, i) => <ArtRow name={x} key={i} helper={i} />);
+
+// lol this is hilarious inefficient
 
 export default ArtworkGrid;
