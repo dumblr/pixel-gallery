72137b38b9b53d3e648ca7ecf441bc84d08343ec
Nick Beattie <nick@nickbytes.com>
2018-08-29 09:33:24 -0400

    refactor: move everything to containers

--- src/App.js
+++ src/App.js
@@ -1,87 +1,57 @@
 import React, { Component } from 'react';
 import { Router } from '@reach/router';
-import urlEnv from './utils/urlEnv';
 import Gallery from './containers/Gallery';
 import Canvas from './containers/Canvas';
 
 class App extends Component {
-  state = {
-    artwork: [],
-    isOwner: false,
-    title: 'pixel-gallery',
-    description: 'a p2p pxon canvas and gallery',
-    siteError: false
-  };
-  async componentDidMount() {
-    try {
-      const archive = await new global.DatArchive(urlEnv());
-      const { title, description } = await archive.getInfo();
-      const artwork = await this.loadArtwork(archive); // get all artwork in artwork folder
+  // state = {
+  //   artwork: [],
+  //   isOwner: false,
+  //   title: "pixel-gallery",
+  //   description: "a p2p pxon canvas and gallery",
+  //   siteError: false
+  // };
+  // async componentDidMount() {
+  //   try {
+  //     const archive = await new global.DatArchive(urlEnv());
+  //     const { title, description } = await archive.getInfo();
+  //     const artwork = await this.loadArtwork(archive); // get all artwork in artwork folder
 
-      this.setState({
-        isOwner: true,
-        artwork,
-        title,
-        description
-      });
-    } catch (error) {
-      // if any of the above results in an error
-      // needs to be refactored
-      // all should be capable of error probably?
-      this.setState({
-        siteError: true,
-        isOwner: false
-      });
-    }
-  }
+  //     this.setState({
+  //       isOwner: true,
+  //       artwork,
+  //       title,
+  //       description
+  //     });
+  //   } catch (error) {
+  //     // if any of the above results in an error
+  //     // needs to be refactored
+  //     // all should be capable of error probably?
+  //     this.setState({
+  //       siteError: true,
+  //       isOwner: false
+  //     });
+  //   }
+  // }
 
-  savePixel = (row, column, color) => {
-    this.setState({
-      [`${row}${column}`]: color
-    });
-  };
+  // loadArtwork = async archive => {
+  //   const art = await archive.readdir("/art");
+  //   if (art.length === 0) {
+  //     return null;
+  //   }
+  //   const promises = art.map(async artwork => {
+  //     const artworkResponse = await archive.readFile(`/art/${artwork}`);
+  //     return JSON.parse(artworkResponse);
+  //   });
 
-  loadArtwork = async archive => {
-    const art = await archive.readdir('/art');
-    if (art.length === 0) {
-      return null;
-    }
-    const promises = art.map(async artwork => {
-      const artworkResponse = await archive.readFile(`/art/${artwork}`);
-      return JSON.parse(artworkResponse);
-    });
-
-    const results = await Promise.all(promises);
-    return results;
-  };
-
-  createArtwork = () => {
-    // creates art file in art folder
-  };
-
-  saveArtworkAsDraft = () => {
-    // saves to drafts folder
-  };
-
-  loadSavedFromDraft = () => {
-    // load from drafts folder into editor
-    // display drafts in a list on the canvas page
-  };
-
-  deleteArtwork = () => {
-    // deletes new art files
-    // ability to do this in own gallery or a draft
-  };
+  //   const results = await Promise.all(promises);
+  //   return results;
+  // };
 
   render() {
     return (
       <Router>
-        <Gallery
-          path="/"
-          title={this.state.title}
-          description={this.state.description}
-          artwork={this.state.artwork}
-        />
+        <Gallery path="/" />
         <Canvas path="/canvas" />
       </Router>
     );
--- src/containers/Gallery/index.js
+++ src/containers/Gallery/index.js
@@ -1,13 +1,62 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/Header';
 import ArtworkList from '../../components/ArtworkList';
+import urlEnv from '../../utils/urlEnv';
 
-const Gallery = ({ title, description, artwork }) => (
-  <Fragment>
-    {console.log(artwork)}
-    <Header title={title} description={description} />
-    <ArtworkList artwork={artwork} />
-  </Fragment>
-);
+class Gallery extends React.Component {
+  state = {
+    artwork: [],
+    isOwner: false,
+    title: 'pixel-gallery',
+    description: 'a p2p pxon canvas and gallery',
+    siteError: false
+  };
+
+  async componentDidMount() {
+    try {
+      const archive = await new global.DatArchive(urlEnv());
+      const { title, description } = await archive.getInfo();
+      const artwork = await this.loadArtwork(archive); // get all artwork in artwork folder
+
+      this.setState({
+        isOwner: true,
+        artwork,
+        title,
+        description
+      });
+    } catch (error) {
+      // if any of the above results in an error
+      // needs to be refactored
+      // all should be capable of error probably?
+      this.setState({
+        siteError: true,
+        isOwner: false
+      });
+    }
+  }
+
+  loadArtwork = async archive => {
+    const art = await archive.readdir('/art');
+    if (art.length === 0) {
+      return null;
+    }
+    const promises = art.map(async artwork => {
+      const artworkResponse = await archive.readFile(`/art/${artwork}`);
+      return JSON.parse(artworkResponse);
+    });
+
+    const results = await Promise.all(promises);
+    return results;
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Header title={this.state.title} description={this.state.description} />
+        <ArtworkList artwork={this.state.artwork} />
+      </Fragment>
+    );
+  }
+}
 
 export default Gallery;
